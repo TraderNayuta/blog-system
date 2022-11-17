4b8b3ff34ff88d4b7fcdf83e8f3b3955eceed39a
nayuta <tradernayuta@gmail.com>
2022-11-17 23:24:49 +0800

    feat: login page

--- frontend/admin/src/app/views/login/login.component.ts
+++ frontend/admin/src/app/views/login/login.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  passwordHide: Boolean = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      username: [null, Validators.required],
+      password: [null, Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
+
+  getControl(controlName: string): AbstractControl {
+    return this.form.controls[controlName];
+  }
+
+  login() {
+    this.form.markAllAsTouched();
+
+    if (this.form.valid) {
+    }
+  }
 }
